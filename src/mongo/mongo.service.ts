9f94d9c21e96c63541b2ae6573d9c0b2a1a34cb2
Steephen Varghese <31406509+steephenv@users.noreply.github.com>
2022-10-17 17:34:03 +0530

    Add files via upload

--- src/mongo/mongo.service.ts
+++ src/mongo/mongo.service.ts
@@ -1,1 +1,17 @@
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
+export class MongoService {
+    constructor(){
+        console.log("mongo service working");
+    }
+    startConnection(){
+        console.log("start connection");
+    }
+
+    getModels(){
+        return {
+            "flight":{}
+        }
+    }
+}
